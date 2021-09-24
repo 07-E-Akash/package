6e12f0b22f2196b7708617b33361d34aff35a09e
E-07-Akash <akash.gla_cs20@gla.ac.in>
2021-09-24 10:52:57 +0530

    firstirst

---
+++ src/Test.java
@@ -0,0 +1,12 @@
+import p1.demo1;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Test extends  demo1{
+    public static void main(String[] args) {
+        Test obj1 = new Test();
+        System.out.println(obj1.num1+" "+obj1.num2);
+
+    }
+}
---
+++ src/p1/demo1.java
@@ -0,0 +1,20 @@
+package p1;
+
+public class demo1 {
+    public int num1;
+    public int num2;
+   public demo1()
+    {
+        num1 =10;
+        num2 =20;
+
+
+    }
+    public void display()
+    {
+        System.out.println(num1+" "+num2);
+
+    }
+
+
+}
